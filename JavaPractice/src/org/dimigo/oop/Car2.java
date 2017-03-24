5244543b44ecf7ae044422a74e7960bf501e4656
esh7810 <esh7810@DESKTOP-K7VCAOQ>
2017-03-24 11:49:28 +0900

    Car2

--- JavaPractice/src/org/dimigo/oop/Car2.java
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -22,7 +22,7 @@ public class Car2 {
 	private int maxSpeed;
 	private int price; 
 	
-	public Car2(){};
+	public Car2(){}
 	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice){
 		company = newCompany;
 		model = newModel;
